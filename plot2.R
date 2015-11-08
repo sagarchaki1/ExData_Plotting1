7a9fa6463cdf7a9ce789f16aecd60200c3733504
sagarchaki1 <nandini2420@gmail.com>
2015-11-07 21:53:54 -0800

    files

--- plot2.R
+++ plot2.R
@@ -1,8 +1,8 @@
 setwd("~/Nandini/Coursera/Specialization_Data Science/4.Exploratory Data Analysis/Project1")
 
 data1 <- read.table("./exdata_data_household_power_consumption/household_power_consumption.txt",
-                   header=TRUE, sep=";")
-            
+                    header=TRUE, sep=";")
+
 data1$Date <- as.Date(data1$Date,"%d/%m/%Y") 
 
 
@@ -16,16 +16,15 @@ sub.df = subset(data1, Date == '2007-02-01' |
 datetime <- paste(as.Date(sub.df$Date), sub.df$Time)
 sub.df$Datetime <- as.POSIXct(datetime)
 
-par(mfrow=c(1,1))
-##Plot1
+
+##Plot2
 ##By default, the png plots are 480x480 pixels in size
 
 act <- as.numeric(as.character(sub.df$Global_active_power))
 
-png("plot1.png")
-hist(act,xlab="Global Active Power (kilowatts)", main="Global Active Power", col="red", ylim=c(0,1200))
+png("plot2.png")
+plot(act~ sub.df$Datetime, type='l',
+     ylab="Global Active Power (kilowatts)", xlab="",ylim=c(0,8))
 dev.off()
-
-
 
 
--- plot3.R
+++ plot3.R
@@ -1,8 +1,8 @@
 setwd("~/Nandini/Coursera/Specialization_Data Science/4.Exploratory Data Analysis/Project1")
 
 data1 <- read.table("./exdata_data_household_power_consumption/household_power_consumption.txt",
-                   header=TRUE, sep=";")
-            
+                    header=TRUE, sep=";")
+
 data1$Date <- as.Date(data1$Date,"%d/%m/%Y") 
 
 
@@ -12,20 +12,23 @@ sub.df = subset(data1, Date == '2007-02-01' |
 ##2880 rows
 
 
+
 ## Converting dates
 datetime <- paste(as.Date(sub.df$Date), sub.df$Time)
 sub.df$Datetime <- as.POSIXct(datetime)
 
-par(mfrow=c(1,1))
-##Plot1
+##Plot3
 ##By default, the png plots are 480x480 pixels in size
 
-act <- as.numeric(as.character(sub.df$Global_active_power))
+sub1 <- as.numeric(as.character(sub.df$Sub_metering_1))
+sub2 <- as.numeric(as.character(sub.df$Sub_metering_2))
+sub3 <- as.numeric(as.character(sub.df$Sub_metering_3))
 
-png("plot1.png")
-hist(act,xlab="Global Active Power (kilowatts)", main="Global Active Power", col="red", ylim=c(0,1200))
+png("plot3.png")
+plot(sub1~ sub.df$Datetime, type='l',
+     ylab="Energy sub metering", xlab="", ylim=c(0,40))
+lines(sub2~ sub.df$Datetime, type='l',col="red")
+lines(sub3~ sub.df$Datetime, type='l',col="blue")
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,col=c("black","red","blue"))
+
 dev.off()
-
-
-
-
